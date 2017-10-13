e715f4a9d1bf4a09574896587c4bcd616eb634b5
Ricardo Alfaro <ricardoalfarog@gmail.com>
2017-10-13 02:16:04 -0300

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,226 @@
+(function($){
+
+	function initialize() {
+
+	  // Create an array of styles.
+	  var styles = [
+	    {
+	      stylers: [
+	        { hue: "#00ffe6" },
+	        { saturation: -20 }
+	      ]
+	    },{
+	      featureType: "road",
+	      elementType: "geometry",
+	      stylers: [
+	        { lightness: 100 },
+	        { visibility: "simplified" }
+	      ]
+	    },{
+	      featureType: "road",
+	      elementType: "labels",
+	      stylers: [
+	        { visibility: "off" }
+	      ]
+	    }
+	  ];
+
+	  // Create a new StyledMapType object, passing it the array of styles,
+	  // as well as the name to be displayed on the map type control.
+	  var styledMap = new google.maps.StyledMapType(styles,
+	    {name: "Styled Map"});
+
+	  // Create a map object, and include the MapTypeId to add
+	  // to the map type control.
+	  var map = new google.maps.Map(document.getElementById('mapDiv'),
+	    mapOptions);
+
+	  //Associate the styled map with the MapTypeId and set it to display.
+	  map.mapTypes.set('map_style', styledMap);
+	  map.setMapTypeId('map_style');
+	}
+
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(300).fadeOut('slow');
+	});
+
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Smooth scroll / Scroll To Top
+		/* ---------------------------------------------- */
+
+		$('a[href*=#]').bind("click", function(e){
+           
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+
+		/* ---------------------------------------------- /*
+		 * Navbar
+		/* ---------------------------------------------- */
+
+		$('.header').sticky({
+			topSpacing: 0
+		});
+
+		$('body').scrollspy({
+			target: '.navbar-custom',
+			offset: 70
+		})
+
+        
+        /* ---------------------------------------------- /*
+		 * Skills
+        /* ---------------------------------------------- */    
+        //var color = $('#home').css('backgroundColor');
+
+        $('.skills').waypoint(function(){
+            $('.chart').each(function(){
+            $(this).easyPieChart({
+                    size:140,
+                    animate: 2000,
+                    lineCap:'butt',
+                    scaleColor: false,
+                    barColor: '#89B700',
+                    trackColor: 'transparent',
+                    lineWidth: 10
+                });
+            });
+        },{offset:'80%'});
+        
+        
+        /* ---------------------------------------------- /*
+		 * Quote Rotator
+		/* ---------------------------------------------- */
+       
+			$( function() {
+				/*
+				- how to call the plugin:
+				$( selector ).cbpQTRotator( [options] );
+				- options:
+				{
+					// default transition speed (ms)
+					speed : 700,
+					// default transition easing
+					easing : 'ease',
+					// rotator interval (ms)
+					interval : 8000
+				}
+				- destroy:
+				$( selector ).cbpQTRotator( 'destroy' );
+				*/
+
+				$( '#cbp-qtrotator' ).cbpQTRotator();
+
+			} );
+		
+        
+		/* ---------------------------------------------- /*
+		 * Home BG
+		/* ---------------------------------------------- */
+
+		$(".screen-height").height($(window).height());
+
+		$(window).resize(function(){
+			$(".screen-height").height($(window).height());
+		});
+
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+			$('#home').css({'background-attachment': 'scroll'});
+		} else {
+			$('#home').parallax('50%', 0.1);
+		}
+
+
+		/* ---------------------------------------------- /*
+		 * WOW Animation When You Scroll
+		/* ---------------------------------------------- */
+
+		wow = new WOW({
+			mobile: false
+		});
+		wow.init();
+
+
+		/* ---------------------------------------------- /*
+		 * E-mail validation
+		/* ---------------------------------------------- */
+
+		function isValidEmailAddress(emailAddress) {
+			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+			return pattern.test(emailAddress);
+		};
+
+		/* ---------------------------------------------- /*
+		 * Contact form ajax
+		/* ---------------------------------------------- */
+
+		$('#contact-form').submit(function(e) {
+
+			e.preventDefault();
+
+			var c_name = $('#c_name').val();
+			var c_email = $('#c_email').val();
+			var c_message = $('#c_message ').val();
+			var response = $('#contact-form .ajax-response');
+
+			if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
+				response.fadeIn(500);
+				response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
+			}
+
+			else {				
+				    $('#contact-form .ajax-hidden').fadeOut(500);
+				    response.html("Message Sent. I will contact you asap. Thanks.").fadeIn(500);
+				}
+            
+            	return false;
+			});
+
+	});
+	
+	$(document).ready(function() {
+		$('.iconbox').hover(function(){
+			//$(this).addClass('invisible');
+			$(this).parent().find('.iconbox-text').removeClass('invisible').addClass('animated');
+			$(this).parent().find('.iconbox-text').fadeIn('1000');
+		});
+
+		$('.iconbox-text').mouseout(function(){
+			$(this).fadeOut('1000');
+			//$(this).parent().find('.iconbox').removeClass('invisible');
+		});
+	});
+
+/*
+
+				jQuery(document).ready(function() {
+	jQuery(".column4_1").mouseover(function(){
+		jQuery(this).find('.icon-box-content').show();
+		
+	});
+
+	jQuery(".icon-box-right .icon-box-content").mouseout(function(){
+		jQuery(this).hide();
+	});
+});			
+	*/
+
+})(jQuery);
